bc64556cc439e6038fd09f0aed309e0c89355b01
emperorvaseline <william.jr.rosmarino@gmail.com>
2020-05-23 16:27:02 +0800

    + Added a Enemy Prefab folder in the Prefabs folder + Using basicSoldier as a template, created the Rusher enemy Prefab and put it into the Enemy Prefab folder + Using basicSoldier as a template, created the Rifler enemy Prefab and put it into the Enemy Prefab folder + Using basicSoldier as a template, created the explosiveSoldier enemy Prefab and put it into the Enemy Prefab folder + Added Rusher script to Enemy Scripts folder within the Script Folder + Added Rifler script to Enemy Scripts folder within the Script Folder + Added explosiveSoldier script to Enemy Scripts folder within the Script Folder + Added RusherDefaultMat to Materials folder + Added RiflerDefaultMat to Materials folder + Added explosiveSolderDefaultMat to Materials folder *capability for fire rate is in the scripts already, but should be tested before going with what the design doc says

---
+++ Assets/Scripts/Enemy AI Scripts/explosiveSoldier.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosiveSoldier : MonoBehaviour
+{
+    public float enemyHealth;
+
+
+    public float enemySpeed;
+    public float enemyStoppingDistance;
+    public float enemyRetreatDistance;
+
+    private float timeBetweenShots;
+    public float startTimeBetweenShots;
+
+    public GameObject bullet;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBetweenShots = startTimeBetweenShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) > enemyStoppingDistance)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
+
+        }
+        else if (Vector3.Distance(transform.position, player.position) < enemyStoppingDistance && Vector3.Distance(transform.position, player.position) > enemyRetreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector3.Distance(transform.position, player.position) < enemyRetreatDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, -enemySpeed * Time.deltaTime);
+        }
+
+        if (timeBetweenShots <= 0)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            timeBetweenShots = startTimeBetweenShots;
+        }
+        else
+        {
+            timeBetweenShots -= Time.deltaTime;
+        }
+
+        if (enemyHealth == 0.0f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
